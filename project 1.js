cf04e478dddb1358fb8b9df725e79788782500de
Shreyas539 <131728495+Shreyas539@users.noreply.github.com>
2023-06-25 07:31:47 -0400

    Add files via upload

---
+++ project 1.js
@@ -0,0 +1,13 @@
+
+
+
+function turnOn()
+{
+    document.getElementById('image').src = 'pic_bulbon.gif';
+    return 1;
+}
+
+function turnOFF()
+{
+    document.getElementById('image').src = 'pic_bulboff.gif' ;
+}
